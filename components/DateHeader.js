cbd79d9c8b0bfc8320347d07daaf05d876ddcb03
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-04-13 23:01:23 -0700

    feat: DateHeader got new make over

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {purple} from "../utils/colors";
 
 function DateHeader({date}) {
     return (
-        <Text>{date}</Text>
+        <Text style={{color: purple, fontSize: 25, marginTop: 10}}>{date}</Text>
     );
 }
+
 
 export default DateHeader;
